be5ad71d7cac95276a0ff5ef7d20c81c6dff1f04
Mikhail Bulash <scaryspiderpig@gmail.com>
2014-09-14 19:21:55 +0300

    load & save async tasks

--- app/src/main/kotlin/vu/co/mikeroll/ebony/app/TodoListFragment.kt
+++ app/src/main/kotlin/vu/co/mikeroll/ebony/app/TodoListFragment.kt
@@ -2,20 +2,38 @@ package vu.co.mikeroll.ebony.app
 
 import android.os.Bundle
 import android.app.ListFragment
-import vu.co.mikeroll.ebony.data.TodoList
-import org.json.JSONArray
+import vu.co.mikeroll.ebony.db.Database
+import android.os.AsyncTask
+import android.database.Cursor
+import vu.co.mikeroll.ebony.db.TodoItem
 
 public class TodoListFragment() : ListFragment() {
 
+    var adapter: TodoListAdapter? = null
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        val input : String = """
-          [
-             { "c": "Entry 1\nlol\nlol\nlol", "i": false },
-             { "c": "Entry 2", "i": true },
-             { "c": "Entry 3" }
-          ]
-        """
-        setListAdapter(TodoListAdapter(getActivity()!!, TodoList(JSONArray(input: String?))))
+        val ctx = getActivity()!!
+        Database.connect(ctx)
+        adapter = TodoListAdapter(ctx)
+        setListAdapter(adapter)
+        load()
     }
+
+
+    fun load() = object : AsyncTask<Void, Void, Cursor>() {
+        override fun doInBackground(vararg params: Void?): Cursor? {
+            return Database.selectAll()
+        }
+        override fun onPostExecute(result: Cursor) {
+            adapter?.changeCursor(result)
+        }
+    }.execute()
+
+    fun save(item: TodoItem) = object : AsyncTask<TodoItem, Void, Void>() {
+        override fun doInBackground(vararg items: TodoItem?): Void? {
+            Database.upsert(items[0]!!)
+            return null
+        }
+    }.execute(item)
 }
