5beee6c0cde39ee970a7722a23a19920834d3f9a
Mikhail Bulash <scaryspiderpig@gmail.com>
2014-09-16 21:10:25 +0300

    make TodoItem Parcelable

--- app/src/main/kotlin/vu/co/mikeroll/ebony/db/TodoItem.kt
+++ app/src/main/kotlin/vu/co/mikeroll/ebony/db/TodoItem.kt
@@ -1,5 +1,24 @@
 package vu.co.mikeroll.ebony.db
 
-public data class TodoItem(var content: String, var important: Boolean) {
-    var id: Long = -1
+import android.os.Parcelable
+import android.os.Parcel
+
+public data class TodoItem(var content: String, var important: Boolean, var id: Long = -1) : Parcelable {
+
+    override fun describeContents() = 0
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeString(content)
+        dest.writeByte(if (important) 1 else 0)
+        dest.writeLong(id)
+    }
+
+    class object {
+        val CREATOR = object : Parcelable.Creator<TodoItem> {
+            override fun createFromParcel(p: Parcel) =
+                TodoItem(p.readString(), p.readByte() != 0.toByte(), p.readLong())
+
+            override fun newArray(size: Int) = Array(size, { TodoItem("", false) })
+        }
+    }
 }
